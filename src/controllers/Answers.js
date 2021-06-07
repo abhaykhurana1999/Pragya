812dd9f9daa6ffc061477c6c86d6af7da497e0e9
Abhay <Abhaykhurana1999@gmail.com>
2021-06-07 12:16:31 +0530

    Updating Controllers
    Signed-off-by: Abhay <Abhaykhurana1999@gmail.com>

---
+++ src/controllers/Answers.js
@@ -0,0 +1,24 @@
+const { Answers } = require('../db/models')
+
+async function createNewA(id,body)
+{
+    const answer= await Answers.create({ id,body})
+
+    return answer
+}
+
+async function showAllAnswers(query)
+{
+    // TODO::HANDLE QUERY PARAMS
+    const a= await Answers.findAll()
+    return a;
+}
+
+ 
+
+/*async function task()
+{
+    console.log( await createNewA(1,'Hello Answers'))
+}
+
+task()*/
